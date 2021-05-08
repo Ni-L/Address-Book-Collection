7ec899ef1760e56ca282b2996863f303f0caf6e1
unknown <nilimawadal1@gmail.com>
2021-05-08 18:54:33 +0530

    [Nilima] Added Ability to view city state

---
+++ Address-Book-Collection/Address-Book-Collection/IPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_Collection
+{/// <summary>
+ /// signature of interface
+ /// Calling Methods
+ /// </summary>
+    public interface IPerson
+    {
+        void displayPerson();
+        void editPerson();
+        void deletePerson();
+    }
+}
